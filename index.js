da0a529a429bcee529e82a500464a69f2ddc3abd
joaquin <jecorrea@protonmail.com>
2021-05-14 09:03:52 -0400

    commit tree fbbcd7e2b1a184382f27cb6adc41d31b5006327f

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+class Dog{
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }}
+
+class Cat{
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Bird{
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        let message
+        this.sex === `male` ? message = `It's me! ${this.name}, the parrot!` : message = `${this.name} says squawk!`
+        return message
+    }
+}
